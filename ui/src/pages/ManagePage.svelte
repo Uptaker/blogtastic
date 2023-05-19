d40a32dcc5a75501bfedf10c97360259b4b62d37
Uptaker <markus.tammeoja@gmail.com>
2023-05-19 21:47:57 +0300

    #5 post selection setup

--- ui/src/pages/ManagePage.svelte
+++ ui/src/pages/ManagePage.svelte
@@ -3,6 +3,7 @@
     import SortableTable from "src/components/SortableTable.svelte";
     import type {Post} from "src/api/types";
     import api from "src/api/api";
+    import Card from "src/components/Card.svelte";
 
     let posts: Post[]
 
@@ -14,10 +15,23 @@
 </script>
 
 <MainPageLayout title="Manage posts">
-    <SortableTable items={posts} columns={['title', 'Creation Date']} let:item={p} class="w-full">
-        <tr>
-            <td>{p.title}</td>
-            <td>{new Date(p.createdAt).toLocaleString()}</td>
-        </tr>
-    </SortableTable>
+    <div class="grid grid-template gap-1">
+        <Card padding="p-1">
+            <SortableTable items={posts} columns={['title', 'Creation Date']} let:item={p} class="w-full">
+                <tr class="text-sm">
+                    <td>{p.title}</td>
+                    <td>{new Date(p.createdAt).toLocaleString()}</td>
+                </tr>
+            </SortableTable>
+        </Card>
+        <Card>
+            Select a post to start editing
+        </Card>
+    </div>
 </MainPageLayout>
+
+<style>
+    .grid-template {
+        grid-template-columns: 1fr 4fr;
+    }
+</style>
