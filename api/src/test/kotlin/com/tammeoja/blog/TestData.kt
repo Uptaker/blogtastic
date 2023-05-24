28b180496dcda790f79c9c0aa693dd79ed6a4e5e
Uptaker <markus.tammeoja@gmail.com>
2023-05-25 01:00:05 +0300

    update TestData.kt

--- api/src/test/kotlin/com/tammeoja/blog/TestData.kt
+++ api/src/test/kotlin/com/tammeoja/blog/TestData.kt
@@ -1,8 +1,10 @@
 import com.tammeoja.blog.posts.Post
+import com.tammeoja.blog.user.User
 import java.time.LocalDate
 
 val today = LocalDate.now()
 val tomorrow = today.plusDays(1)
 val yesterday = today.minusDays(1)
 
-val post = Post("Awesome Post!", "Hello! This is my post!\n\nFeel free to leave a comment!")
+val user = User("Markus", "Tammeoja", "m@t.com", "123123123")
+val post = Post("Awesome Post!", "Hello! This is my post!\n\nFeel free to leave a comment!", "/awesome", user.id)
