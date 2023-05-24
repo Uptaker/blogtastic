b99e9baa9bff4e9d14e0a139073bc534f23e0e39
Uptaker <markus.tammeoja@gmail.com>
2023-05-25 00:33:55 +0300

    #7 remove startup password log

--- api/src/main/kotlin/com/tammeoja/blog/security/SecurityConfig.kt
+++ api/src/main/kotlin/com/tammeoja/blog/security/SecurityConfig.kt
@@ -6,8 +6,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 
@@ -25,5 +27,9 @@ class SecurityConfig {
     .build()
 
   @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+  // TODO - this does nothing and its only purpose is to remove default log. find a better way
+  @Bean fun userDetailsService(): UserDetailsService? = InMemoryUserDetailsManager()
+
 
 }
