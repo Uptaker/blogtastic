12e01a87d4ed00edb5616b6ec95b887c8c240e4a
Uptaker <markus.tammeoja@gmail.com>
2023-05-27 01:14:14 +0300

    #33 post page styles

--- ui/src/pages/PostPage.svelte
+++ ui/src/pages/PostPage.svelte
@@ -3,6 +3,7 @@
     import type {Post} from "src/api/types";
     import api from "src/api/api";
     import {marked} from "marked";
+    import Card from "src/components/Card.svelte";
 
     export let slug: string
     let post: Post
@@ -19,9 +20,11 @@
             <div class="h-12 flex justify-center items-center py-24 bg-gradient-to-r from-orange-300 to-rose-300">
                 <div class="text-3xl">{post.title}</div>
             </div>
-            <div class="w-8/12 mx-auto">
-                <div class="my-4">{post.subheadline}</div>
-                <div>{@html marked.parse(post.content)}</div>
+            <div class="xl:w-8/12 mx-auto">
+                <div class="my-16 text-primary-800">{post.subheadline}</div>
+                <Card>
+                    <div class="markdown">{@html marked.parse(post.content)}</div>
+                </Card>
             </div>
 
         {/if}
